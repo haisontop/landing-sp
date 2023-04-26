bcf40d441616a16ca4ab6e2d811a8726146e8a7d
haisontop <haisontop@gmail.com>
2023-04-26 11:49:04 -0700

    fix 2xl container width custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,24 +12,27 @@ module.exports = {
         current: "currentColor",
         "sp-pink": "#F2E3E1",
         "sp-blue": "#21364F",
-        "unnamed-color" : "#042134",
-        "unnamed-gray" : "#F0F2F6",
-        "sp-gray" : "#D3D3D3",
-        "sp-black" : "#222222",
-        "sp-solid-blue" : {
-          "800" : "#011E2D",
-          "700" : "#1C2F44",
-          "500" : "#3A5877"
+        "unnamed-color": "#042134",
+        "unnamed-gray": "#F0F2F6",
+        "sp-gray": "#D3D3D3",
+        "sp-black": "#222222",
+        "sp-solid-blue": {
+          800: "#011E2D",
+          700: "#1C2F44",
+          500: "#3A5877",
         },
-        "sp-solid-gray" : {
-          "500" : "#F0F2F6",
-          "700" : "#EEF0F4",
-          "600" : "#D3D3D3"
-        }
+        "sp-solid-gray": {
+          500: "#F0F2F6",
+          700: "#EEF0F4",
+          600: "#D3D3D3",
+        },
+      },
+    },
+    container: {
+      screens: {
+        "2xl": "1640px",
       },
     },
   },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
